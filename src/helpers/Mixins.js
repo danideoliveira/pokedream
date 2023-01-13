3370e1add358c4a216c004cd2f5cc8dc55e6f092
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-13 20:47:33 -0300

    add flexbox mixin

---
+++ src/helpers/Mixins.js
@@ -0,0 +1,8 @@
+import { css } from "styled-components"
+
+export const setFlexbox = (justifyContent, alignItems, flexDirection) => css`
+    display: flex;
+    justify-content: ${justifyContent};
+    align-items: ${alignItems};
+    flex-direction: ${flexDirection};
+`;
