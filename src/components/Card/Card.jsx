28d28d949ae53de7ff971e1457ad71f82c75d33b
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-14 17:46:56 -0300

    feat: add pokemon type color to container

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
-import { CardContainer, Text } from "./Card.styled";
+import { CardContainer, PokemonTypeContainer, Text } from "./Card.styled";
+import { PokemonConfig } from "../PokemonConfig/PokemonConfig";
 
-export default function Card({ name, weight, types }) {
+export default function Card({ name, weight, pokemonTypes }) {
   return (
     <CardContainer key={name}>
       <div className="div-pokemon-img">
@@ -10,16 +11,27 @@ export default function Card({ name, weight, types }) {
         />
       </div>
       <div className="div-pokemon-name">
-        <Text fontSize="2rem" color="#0c0c0c" fontWeight="700">
-          {name}
+        <Text fontSize="2rem" color="#fff" fontWeight="700">
+          {name.toUpperCase()}
         </Text>
-        <Text fontSize="1.6rem" color="#0c0c0c" fontWeight="400">
+        <Text fontSize="1.6rem" color="#dfdfdf" fontWeight="400">
           Weight: {weight / 10} kg
         </Text>
       </div>
       <div className="div-pokemon-type">
-        {types.map((type) => (
-          <Text key={name}>{type.type.name}</Text>
+        {pokemonTypes.map((pokemonType) => (
+          <PokemonTypeContainer
+            key={PokemonConfig[pokemonType.type.name].typeColor}
+            typeColor={PokemonConfig[pokemonType.type.name].typeColor}
+          >
+            <Text fontSize="1.6rem" color="#dfdfdf" fontWeight="700">
+              {pokemonType.type.name}
+            </Text>
+            <img
+              src={PokemonConfig[pokemonType.type.name].img}
+              alt={`${pokemonType.type.name} icon`}
+            />
+          </PokemonTypeContainer>
         ))}
       </div>
     </CardContainer>
--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -5,15 +5,18 @@ export const CardContainer = styled.div`
   ${setFlexbox("center", "center", "column")};
   /* width: 200px; */
   width: 100%;
-  height: 250px;
+  height: 300px;
   grid-row-gap: 10px;
-  background-color: #a9a9a9;
+  background-color: #1e1e1e;
   padding: 10px;
   border-radius: 10px;
 
   .div-pokemon-img {
     ${setFlexbox("center", "center", "column")};
     height: 70%;
+    width: 70%;
+    background-color: white;
+    clip-path: circle();
   }
 
   .div-pokemon-name {
@@ -26,11 +29,29 @@ export const CardContainer = styled.div`
     ${setFlexbox("center", "center", "row")};
     height: 10%;
     gap: 10px;
+
+    img {
+      width: 20px;
+    }
   }
 `;
 
+export const PokemonTypeContainer = styled.div`
+  ${setFlexbox("center", "center", "row")};
+  width: 100%;
+  height: 100%;
+  gap: 5px;
+  background-color: ${(props) => props.typeColor};
+  padding: 15px;
+  border-radius: 5px;
+`;
+
 export const Text = styled.p`
-  font-size: ${props => props.fontSize};
-  font-weight: ${props => props.fontWeight};
-  color: ${props => props.color};
+  font-size: ${(props) => props.fontSize};
+  font-weight: ${(props) => props.fontWeight};
+  color: ${(props) => props.color};
+`;
+
+export const PokemonType = styled.img`
+  width: 20px;
 `;
