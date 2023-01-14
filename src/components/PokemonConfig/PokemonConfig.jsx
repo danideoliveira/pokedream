b4b0eb9189377eec81f18e825790d8fd80ce938d
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-14 17:45:33 -0300

    feat: add pokemon config

---
+++ src/components/PokemonConfig/PokemonConfig.jsx
@@ -0,0 +1,10 @@
+import { images } from "../Images/Images";
+
+export const PokemonConfig = {
+  bug: { name: "bug", typeColor: "", img: images.bug },
+  dark: { name: "dark", typeColor: "#5fbd58", img: images.dark },
+  grass: { name: "grass", typeColor: "#5fbd58", img: images.grass },
+  fire: { name: "fire", typeColor: "#fba54c", img: images.fire },
+  poison: { name: "poison", typeColor: "#b763cf", img: images.poison },
+  flying: { name: "flying", typeColor: "#a1bbec", img: images.flying },
+};
