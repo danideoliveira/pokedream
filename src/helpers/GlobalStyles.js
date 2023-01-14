d0b77ad7d9a2069b2391deb0261ae9b6fe049400
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-13 21:21:45 -0300

    Add global styles

---
+++ src/helpers/GlobalStyles.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+    text-decoration: none;
+  }
+
+  html {
+    font-size: 64.7%;
+  }
+`;
