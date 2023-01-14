14a3e0cfc36a581f77c7330fe60bc6b529740aac
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-14 17:38:43 -0300

    feat: add images component

---
+++ src/components/Images/Images.jsx
@@ -0,0 +1,15 @@
+import bug from "../../assets/img/bug.svg";
+import dark from "../../assets/img/dark.svg";
+import grass from "../../assets/img/grass.svg";
+import fire from "../../assets/img/fire.svg";
+import poison from "../../assets/img/poison.svg";
+import flying from "../../assets/img/flying.svg";
+
+export const images = {
+  bug,
+  dark,
+  grass,
+  fire,
+  poison,
+  flying,
+};
