99bcc7fdcec6348632e03911c8d7c3b3da857fac
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-19 17:38:38 -0300

    feat: add new icons

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -1,9 +1,15 @@
+// Pokemon Types Icons
 import bug from "../../assets/img/bug.svg";
 import dark from "../../assets/img/dark.svg";
 import grass from "../../assets/img/grass.svg";
 import fire from "../../assets/img/fire.svg";
 import poison from "../../assets/img/poison.svg";
 import flying from "../../assets/img/flying.svg";
+
+// Icons
+import iconWeight from "../../assets/img/iconWeight.svg";
+import iconHeight from "../../assets/img/iconHeight.svg";
+import iconTopArrow from "../../assets/img/iconTopArrow.svg";
 
 export const images = {
   bug,
@@ -12,4 +18,7 @@ export const images = {
   fire,
   poison,
   flying,
+  iconWeight,
+  iconHeight,
+  iconTopArrow
 };
