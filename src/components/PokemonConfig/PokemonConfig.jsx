8c17159f865bdd27ead8b4775e1f869bd0679c77
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-19 21:04:45 -0300

    feat: add new pokemon configs

--- src/components/PokemonConfig/PokemonConfig.jsx
+++ src/components/PokemonConfig/PokemonConfig.jsx
@@ -1,8 +1,12 @@
 import { images } from "../Images/Images";
 
 export const PokemonConfig = {
-  bug: { name: "bug", typeColor: "", img: images.bug },
-  dark: { name: "dark", typeColor: "#5fbd58", img: images.dark },
+  bug: {
+    name: "bug",
+    typeColor: "#596700",
+    gradientColor: "linear-gradient(180deg, #50FF81 0%, #596700 100%)",
+    img: images.bug,
+  },
   grass: {
     name: "grass",
     typeColor: "#5fbd58",
@@ -11,10 +15,86 @@ export const PokemonConfig = {
   },
   fire: {
     name: "fire",
-    typeColor: "#fba54c",
+    typeColor: "#FF5F2D",
     gradientColor: "linear-gradient(180deg, #EFA83D 0%, #FF5F2D 100%);",
     img: images.fire,
   },
-  poison: { name: "poison", typeColor: "#b763cf", img: images.poison },
-  flying: { name: "flying", typeColor: "#a1bbec", img: images.flying },
+  water: {
+    name: "water",
+    typeColor: "#0072B1",
+    gradientColor: "linear-gradient(180deg, #50DFFF 0%, #0072B1 100%);",
+    img: images.water,
+  },
+  poison: {
+    name: "poison",
+    typeColor: "#b763cf",
+    gradientColor: "linear-gradient(180deg, #FF5AEF 0%, #C213FF 100%)",
+    img: images.poison,
+  },
+  ghost: {
+    name: "ghost",
+    typeColor: "#4F00B5",
+    gradientColor: "linear-gradient(180deg, #905CFF 0%, #4F00B5 100%)",
+    img: images.ghost,
+  },
+  dragon: {
+    name: "dragon",
+    typeColor: "#CB5CFF",
+    gradientColor: "linear-gradient(180deg, #42A4FF 0%, #CB5CFF 100%)",
+    img: images.dragon,
+  },
+  electric: {
+    name: "electric",
+    typeColor: "#FFDA58",
+    gradientColor: "linear-gradient(180deg, #FFDA58 0%, #CA9D00 100%)",
+    img: images.electric,
+  },
+  ice: {
+    name: "ice",
+    typeColor: "#63B4FF",
+    gradientColor: "linear-gradient(180deg, #B0ECFF 0%, #63B4FF 100%)",
+    img: images.ice,
+  },
+  flying: {
+    name: "flying",
+    typeColor: "#4E6383",
+    gradientColor: "linear-gradient(180deg, #8ED6FF 0%, #4E6383 100%)",
+    img: images.flying,
+  },
+  ground: {
+    name: "ground",
+    typeColor: "#AF6900",
+    gradientColor: "linear-gradient(180deg, #FFCC4A 0%, #AF6900 100%)",
+    img: images.ground,
+  },
+  rock: {
+    name: "rock",
+    typeColor: "#703C26",
+    gradientColor: "linear-gradient(180deg, #FFAC4B 0%, #703C26 98.44%)",
+    img: images.rock,
+  },
+  fighting: {
+    name: "fighting",
+    typeColor: "#AA1209",
+    gradientColor: "linear-gradient(180deg, #FF5858 0%, #AA1209 100%)",
+    img: images.fighting,
+  },
+  psychic: {
+    name: "psychic",
+    typeColor: "#FF239A",
+    gradientColor: "linear-gradient(180deg, #FFA9F1 0%, #FF239A 98.44%)",
+    img: images.psychic,
+  },
+  steel: {
+    name: "steel",
+    typeColor: "#535353",
+    gradientColor: "linear-gradient(180deg, #D9D9D9 0%, #535353 100%)",
+    img: images.steel,
+  },
+  normal: {
+    name: "normal",
+    typeColor: "#6B5B33",
+    gradientColor: "linear-gradient(180deg, #FFF1BF 0%, #6B5B33 98.44%)",
+    img: images.normal,
+  },
 };
