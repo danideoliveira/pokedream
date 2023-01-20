53a1d82fde44b9791540e7af9bb9f402e7837f53
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-19 21:03:48 -0300

    feat: add male and female icon

--- src/components/Images/Images.jsx
+++ src/components/Images/Images.jsx
@@ -1,24 +1,48 @@
 // Pokemon Types Icons
 import bug from "../../assets/img/bug.svg";
-import dark from "../../assets/img/dark.svg";
 import grass from "../../assets/img/grass.svg";
 import fire from "../../assets/img/fire.svg";
 import poison from "../../assets/img/poison.svg";
 import flying from "../../assets/img/flying.svg";
+import water from "../../assets/img/water.svg";
+import ghost from "../../assets/img/ghost.svg";
+import normal from "../../assets/img/normal.svg";
+import steel from "../../assets/img/steel.svg";
+import dragon from "../../assets/img/dragon.svg";
+import psychic from "../../assets/img/psychic.svg";
+import ground from "../../assets/img/ground.svg";
+import rock from "../../assets/img/rock.svg";
+import fighting from "../../assets/img/fighting.svg";
+import electric from "../../assets/img/electric.svg";
+import ice from "../../assets/img/ice.svg";
 
 // Icons
 import iconWeight from "../../assets/img/iconWeight.svg";
 import iconHeight from "../../assets/img/iconHeight.svg";
 import iconTopArrow from "../../assets/img/iconTopArrow.svg";
+import iconMale from "../../assets/img/iconMale.svg";
+import iconFemale from "../../assets/img/iconFemale.svg";
 
 export const images = {
   bug,
-  dark,
   grass,
   fire,
   poison,
   flying,
   iconWeight,
   iconHeight,
-  iconTopArrow
+  iconTopArrow,
+  iconMale,
+  iconFemale,
+  water,
+  ghost,
+  normal,
+  steel,
+  dragon,
+  psychic,
+  ground,
+  rock,
+  fighting,
+  electric,
+  ice,
 };
