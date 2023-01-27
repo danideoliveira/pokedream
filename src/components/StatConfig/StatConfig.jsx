3939806399faaa0b68f6c98570fa9103106d2eab
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-01-27 18:17:49 -0300

    feat: create stat config component

---
+++ src/components/StatConfig/StatConfig.jsx
@@ -0,0 +1,28 @@
+import { colors } from "../../helpers/ColorPalette";
+
+export const StatConfig = {
+  hp: {
+    name: "HP",
+    color: colors.statsHp,
+  },
+  attack: {
+    name: "ATK",
+    color: colors.statsAtk,
+  },
+  defense: {
+    name: "DEF",
+    color: colors.statsDef,
+  },
+  specialattack: {
+    name: "SP.ATK",
+    color: colors.statsSpAtk,
+  },
+  specialdefense: {
+    name: "SP.DEF",
+    color: colors.statsSpDef,
+  },
+  speed: {
+    name: "SPEED",
+    color: colors.statsSpeed,
+  },
+};
