8f376dc5d9e6d801bd55eeab8784529800741e6f
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-02 16:11:40 -0300

    feat: add gradient and description component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,13 @@ import {
   PokemonImage,
   RightContent,
   Title,
+  Gradient,
+  BoxTitle,
+  Description,
 } from "./Home.styled";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { PokemonConfig } from "../../components/PokemonConfig/PokemonConfig";
 
 export default function Home() {
   const [pokemonId, setPokemonId] = useState();
@@ -43,14 +47,30 @@ export default function Home() {
       {pokemon.map((currentPokemon) => (
         <Container key={currentPokemon.name}>
           <LeftContent>
+            <Gradient
+              background={
+                PokemonConfig[currentPokemon.types[0].type.name].gradientColor
+              }
+            />
             <PokemonImage
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemonId}.png`}
               alt={`pokemon`}
             />
           </LeftContent>
           <RightContent>
-            <Title>{currentPokemon.types[0].type.name} pokemons</Title>
-            <p>Teste</p>
+            <BoxTitle>
+              <Title>
+                {currentPokemon.types[0].type.name.charAt(0).toUpperCase() +
+                  currentPokemon.types[0].type.name.slice(1)}{" "}
+                pokemons
+              </Title>
+              <Description>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio,
+                velit rem consectetur magni facilis fuga odio nemo aspernatur
+                nam id laboriosam harum, sunt minus quo excepturi est? Modi,
+                cupiditate necessitatibus?
+              </Description>
+            </BoxTitle>
           </RightContent>
         </Container>
       ))}
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -11,22 +11,51 @@ export const Container = styled.div`
 
 export const LeftContent = styled.div`
   ${setFlexbox("center", "center", "row")}
+  position: relative;
   width: 50%;
   height: 100%;
+  z-index: 1;
 `;
 
 export const RightContent = styled.div`
   ${setFlexbox("center", "center", "column")}
   width: 50%;
   height: 100%;
+  z-index: 1;
+`;
+
+export const BoxTitle = styled.div`
+  ${setFlexbox("center", "flex-start", "column")}
+  width: 70%;
+  height: auto;
 `;
 
 export const Title = styled.h1`
   font-size: 5.6rem;
   font-weight: 700;
   color: ${colors.homeTitleColor};
+  line-height: 5rem;
+  margin-bottom: 15px;
+  width: 50%;
+`;
+
+export const Description = styled.p`
+  font-size: 1.8rem;
+  font-weight: 500;
+  text-align: justify;
+  color: ${colors.homeDescriptionColor};
 `;
 
 export const PokemonImage = styled.img`
   width: 35rem;
+  z-index: 1;
+`;
+
+export const Gradient = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: ${(props) => props.background};
+  z-index: 0;
+  clip-path: polygon(0% 100%, 0% 0%, 0% 0%, 100% 0%);
 `;
