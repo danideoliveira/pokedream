3c363a33374e6f84afb349d6cb40eb9ec716e9f9
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-02 19:51:16 -0300

    feat: add new card style

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
 import {
   CardContainer,
+  LeftContent,
   PokemonImageContainer,
   PokemonTypeContainer,
+  RightContent,
   Text,
 } from "./Card.styled";
 import { PokemonConfig } from "../PokemonConfig/PokemonConfig";
@@ -58,72 +60,59 @@ export default function Card({
   };
 
   return (
-    <CardContainer key={name}>
-      <PokemonImageContainer
-        background={PokemonConfig[pokemonTypes[0].type.name].gradientColor}
-      >
-        <img
-          src={`https://play.pokemonshowdown.com/sprites/ani/${pokemonVerifyUrl(
-            name
-          )}.gif`}
-          alt={`${name} gif`}
-        />
-      </PokemonImageContainer>
-      <div className="div-pokemon-name">
-        <Text fontSize="2.2rem" color={colors.cardText} fontWeight="700">
-          {pokemonVerifyName(name)}
-        </Text>
-        <Text fontSize="2rem" color={colors.cardId} fontWeight="700">
-          #{id}
-        </Text>
-      </div>
-      <div className="div-pokemon-type">
-        {pokemonTypes.map((pokemonType) => (
-          <PokemonTypeContainer
-            key={PokemonConfig[pokemonType.type.name].typeColor}
-            typeColor={PokemonConfig[pokemonType.type.name].gradientColor}
-          >
-            <Text fontSize="1.8rem" color={colors.cardText} fontWeight="700">
-              {pokemonType.type.name}
-            </Text>
-            <img
-              src={PokemonConfig[pokemonType.type.name].img}
-              alt={`${pokemonType.type.name} icon`}
-            />
-          </PokemonTypeContainer>
-        ))}
-      </div>
-      <div className="div-pokemon-base">
-        <div className="pokemon-weight">
-          <img src={images.iconWeight} alt="logo" />
-          <Text fontSize="1.8rem" color={colors.cardText} fontWeight="700">
-            {weight / 10} kg
-          </Text>
-        </div>
-        <div className="pokemon-height">
-          <img src={images.iconHeight} alt="logo" />
-          <Text fontSize="1.8rem" color={colors.cardText} fontWeight="700">
-            {height / 10} m
-          </Text>
-        </div>
-      </div>
-      <div className="div-show-stats">
-        <button
-          onClick={() =>
-            handleOpenModal({
-              name,
-              id,
-              weight,
-              height,
-              stats,
-              pokemonTypes,
-              isCompareFull,
-            })
-          }
+    <CardContainer
+      key={name}
+      onClick={() =>
+        handleOpenModal({
+          name,
+          id,
+          weight,
+          height,
+          stats,
+          pokemonTypes,
+          isCompareFull,
+        })
+      }
+    >
+      <LeftContent>
+        <PokemonImageContainer
+          background={PokemonConfig[pokemonTypes[0].type.name].gradientColor}
         >
-          <img src={images.iconTopArrow} />
-        </button>
-      </div>
+          <img
+            src={`https://play.pokemonshowdown.com/sprites/ani/${pokemonVerifyUrl(
+              name
+            )}.gif`}
+            alt={`${name} gif`}
+          />
+        </PokemonImageContainer>
+      </LeftContent>
+      <RightContent>
+        <div className="div-pokemon-name">
+          <Text fontSize="2.2rem" color={colors.cardText} fontWeight="700">
+            {pokemonVerifyName(name)}
+          </Text>
+          <Text fontSize="2rem" color={colors.cardId} fontWeight="700">
+            #{id}
+          </Text>
+        </div>
+        <div className="div-pokemon-type">
+          {pokemonTypes.map((pokemonType) => (
+            <PokemonTypeContainer
+              key={PokemonConfig[pokemonType.type.name].typeColor}
+              typeColor={PokemonConfig[pokemonType.type.name].gradientColor}
+            >
+              <Text fontSize="1.8rem" color={colors.cardText} fontWeight="700">
+                {pokemonType.type.name}
+              </Text>
+              <img
+                src={PokemonConfig[pokemonType.type.name].img}
+                alt={`${pokemonType.type.name} icon`}
+              />
+            </PokemonTypeContainer>
+          ))}
+        </div>
+        
+      </RightContent>
     </CardContainer>
   );
 }
--- src/components/Card/Card.styled.js
+++ src/components/Card/Card.styled.js
@@ -3,14 +3,20 @@ import { setFlexbox } from "../../helpers/Mixins";
 import { colors } from "../../helpers/ColorPalette";
 
 export const CardContainer = styled.div`
-  ${setFlexbox("center", "center", "column")};
-  width: 300px;
-  height: 415px;
+  ${setFlexbox("center", "center", "row")};
+  width: 380px;
+  height: 150px;
   grid-row-gap: 10px;
   background-color: ${colors.cardBackground};
-  padding: 10px 10px 0 10px;
-  border-radius: 10px 10px 20px 20px;
+  /* padding: 10px; */
+  border-radius: 5px;
   overflow: hidden;
+  cursor: pointer;
+  transition: 0.1s;
+
+  &:hover {
+    background: ${colors.cardBackgroundHover};
+  }
 
   .div-pokemon-img {
     ${setFlexbox("center", "center", "column")};
@@ -22,14 +28,16 @@ export const CardContainer = styled.div`
 
   .div-pokemon-name {
     ${setFlexbox("center", "center", "column")};
-    height: 20%;
+    /* height: 20%; */
     gap: 2px;
+    margin-bottom: 2rem;
   }
 
   .div-pokemon-type {
     ${setFlexbox("center", "center", "row")};
-    height: 10%;
+    /* height: 10%; */
     gap: 10px;
+    height: 2.5rem;
 
     img {
       width: 2rem;
@@ -70,12 +78,24 @@ export const CardContainer = styled.div`
   }
 `;
 
+export const LeftContent = styled.div`
+  ${setFlexbox("center", "center", "row")};
+  width: 35%;
+  height: 100%;
+`;
+
+export const RightContent = styled.div`
+  ${setFlexbox("center", "center", "column")};
+  width: 70%;
+  height: 100%;
+`;
+
 export const PokemonImageContainer = styled.div`
   ${setFlexbox("center", "center", "column")};
-  height: 70%;
-  width: 70%;
+  height: 100%;
+  width: 100%;
   background: ${(props) => props.background};
-  clip-path: circle();
+  /* clip-path: circle(); */
 `;
 
 export const PokemonTypeContainer = styled.div`
