9be29dcca312d9bc3fea1038a9d14268e3fe79b4
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-10 23:34:04 -0300

    feat: create verify pokemon helper

---
+++ src/helpers/VerifyPokemon.jsx
@@ -0,0 +1,28 @@
+import { images } from "../components/Images/Images";
+
+export const verifyPokemon = {
+  nidoranm: {
+    newName: "nidoran",
+    urlName: "nidoran",
+    img: images.iconMale,
+    altText: "icon male",
+  },
+  nidoranf: {
+    newName: "nidoran",
+    urlName: "nidoran-f",
+    img: images.iconFemale,
+    altText: "icon female",
+  },
+  hooh: {
+    newName: "ho-oh",
+    urlName: "hooh",
+  },
+  mrmime: {
+    newName: "mr mime",
+    urlName: "mrmime",
+  },
+  deoxysnormal: {
+    newName: "deoxys",
+    urlName: "deoxys",
+  }
+};
