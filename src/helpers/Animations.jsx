98b9c955724674ef834fe5cb20af38b989d01f0e
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-12 18:14:26 -0300

    feat: create animations file

---
+++ src/helpers/Animations.jsx
@@ -0,0 +1,21 @@
+import {keyframes} from "styled-components";
+
+export const AnimationProgressValue = keyframes`
+  0% {
+    width: 0%;
+  }
+  100% {
+    width: 100%;
+  }
+`;
+
+export const FadeLeft = keyframes`
+ 0% {
+  opacity: 0;
+  margin-left: -20px;
+ }
+ 100% {
+  opacity: 1;
+  margin-left: 0;
+ }
+`;
