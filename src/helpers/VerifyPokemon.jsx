e95c28703be506d88d6a3aedb9ee3d1b1eb57935
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-11 21:35:09 -0300

    feat: verify new pokemons

--- src/helpers/VerifyPokemon.jsx
+++ src/helpers/VerifyPokemon.jsx
@@ -24,5 +24,49 @@ export const verifyPokemon = {
   deoxysnormal: {
     newName: "deoxys",
     urlName: "deoxys",
+  },
+  giratinaaltered: {
+    newName: "giratina",
+    urlName: "giratina",
+  },
+  shayminland: {
+    newName: "shaymin",
+    urlName: "shaymin",
+  },
+  mimejr: {
+    newName: "mime jr",
+    urlName: "mimejr",
+  },
+  wormadamplant: {
+    newName: "wormadam",
+    urlName: "wormadam",
+  },
+  basculinredstriped: {
+    newName: "basculin",
+    urlName: "basculin",
+  },
+  darmanitanstandard: {
+    newName: "darmanitan",
+    urlName: "darmanitan",
+  },
+  meloettaaria: {
+    newName: "meloetta",
+    urlName: "meloetta",
+  },
+  keldeoordinary: {
+    newName: "keldeo",
+    urlName: "keldeo",
+  },
+  landorusincarnate: {
+    newName: "landorus",
+    urlName: "landorus",
+  },
+  tornadusincarnate: {
+    newName: "tornadus",
+    urlName: "tornadus",
+  },
+  thundurusincarnate: {
+    newName: "thundurus",
+    urlName: "thundurus",
   }
 };
