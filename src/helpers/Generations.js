0cbcb5c269b9f8ee75fd2eff00d55087eb787231
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-14 20:02:34 -0300

    feat: add generations helper

---
+++ src/helpers/Generations.js
@@ -0,0 +1,12 @@
+export const generations = [
+  { name: "Todas", size: [1, 1008] },
+  { name: "1ª Geração", size: [1, 151] },
+  { name: "2ª Geração", size: [152, 251] },
+  { name: "3ª Geração", size: [252, 386] },
+  { name: "4ª Geração", size: [387, 493] },
+  { name: "5ª Geração", size: [494, 649] },
+  { name: "6ª Geração", size: [650, 721] },
+  { name: "7ª Geração", size: [722, 809] },
+  { name: "8ª Geração", size: [810, 905] },
+  { name: "9ª Geração", size: [906, 1008] },
+];
