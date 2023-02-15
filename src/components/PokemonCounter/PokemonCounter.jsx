5b5f618b2faa760586ebb69d8ab974f6727c5843
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-15 17:46:25 -0300

    feat: add verify number

--- src/components/PokemonCounter/PokemonCounter.jsx
+++ src/components/PokemonCounter/PokemonCounter.jsx
@@ -1,11 +1,13 @@
 import { CounterContainer, CounterText } from "./PokemonCounter.styled";
 
 export default function PokemonCounter({ pokemonCounter }) {
+  const verifyNumber = (pokemonCounter) => {
+    return pokemonCounter > 1 ? "encontrados" : "encontrado";
+  };
+
   return (
-    <CounterContainer>
-      <CounterText>
-        {pokemonCounter} Pokemon encontrados
-      </CounterText>
+    <CounterContainer style={{ display: pokemonCounter < 1 ? "none" : "flex" }}>
+      <CounterText>{pokemonCounter} Pokemon {verifyNumber(pokemonCounter)}</CounterText>
     </CounterContainer>
   );
 }
