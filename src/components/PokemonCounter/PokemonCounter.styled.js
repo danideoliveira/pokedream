2b5b537e33d547476ccac6aee02a9725cf34bc67
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-02-15 17:36:15 -0300

    feat: update counter responsive width

--- src/components/PokemonCounter/PokemonCounter.styled.js
+++ src/components/PokemonCounter/PokemonCounter.styled.js
@@ -4,9 +4,13 @@ import { colors } from "../../helpers/ColorPalette";
 
 export const CounterContainer = styled.div`
   ${setFlexbox("flex-end", "center", "row")}
-  width: 90%;
+  width: 89.4%;
   margin: 0 auto;
   margin-bottom: 3rem;
+
+  @media (min-width: 851px) and (max-width: 1280px) {
+    width: 90%;
+  }
 
   @media (min-width: 320px) and (max-width: 850px) {
     ${setFlexbox("center", "center", "row")}
