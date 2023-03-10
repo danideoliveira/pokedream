89bb11534ec0d60b9aac3c44ea78a9207fd8ca6e
Daniel de Oliveira <daniel.santos.oliveira1981@gmail.com>
2023-03-09 21:37:26 -0300

    feat: add loading points animation

--- src/components/PreLoader/PreLoader.jsx
+++ src/components/PreLoader/PreLoader.jsx
@@ -1,12 +1,26 @@
 import { Loader, LoadingText } from "./PreLoader.styled";
 import PokeballLoading from "../PokeballLoading/PokeballLoading";
+import { useState, useEffect } from "react";
 
 export default function PreLoader({ height, marginText, marginBox }) {
+  const [points, setPoints] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (points.length >= 3) {
+        setPoints("");
+      } else {
+        setPoints(points + ".");
+      }
+    }, 500);
+  }, [points]);
+
   return (
     <Loader style={{ height: height || "100%", margin: marginBox || "0" }}>
       <PokeballLoading />
       <LoadingText style={marginText && { margin: marginText }}>
-        Carregando...
+        Carregando
+        {points}
       </LoadingText>
     </Loader>
   );
